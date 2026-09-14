b4dead34be67a27e2f6f9401041610d34f72e5f0
Mahdi <146871239+Mahdirahimiam@users.noreply.github.com>
2026-09-14 22:58:58 +0330

    Polish portfolio stats and fix lead submission

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./fonts.css";
 import "./header.css";
+import "./home-polish.css";
 import "./case.css";
 import "./admin.css";
 
